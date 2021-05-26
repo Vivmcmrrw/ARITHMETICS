2459cb20eed6e50f70cc708d8e740d0ea93e7cb6
Vivmcmrrw <84195075+Vivmcmrrw@users.noreply.github.com>
2021-05-26 20:02:09 +0700

    Add files via upload

---
+++ NewArithSSRSLY/NewArithSSRSLY.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+
+method1:
+
+
+    float numb1;
+    float numb2;
+    float res;
+    char act1;
+
+    cout << "enter first number, activity (+, -, *, /, ^) and second number  ";
+    cin >> numb1;
+
+    cin >> act1;
+
+    cin >> numb2;
+
+
+    if (act1 == '+') {
+
+        res = numb1 + numb2;
+        cout << " Number 1 + Number 2 = " << res << endl;
+    }
+    else if (act1 == '-') {
+
+        res = numb1 - numb2;
+        cout << " Number 1 - Number 2 = " << res << endl;
+    }
+    else if (act1 == '*') {
+
+        res = numb1 * numb2;
+        cout << " Number 1 * Number 2 = " << res << endl;
+    }
+    else if (act1 == '/') {
+
+        res = numb1 / numb2;
+        cout << " Number 1 / Number 2 = " << res << endl;
+    }
+    else if (act1 == '^') {
+
+        res = pow(numb1, numb2);
+        cout << " Number 1 ^ Number 2 = " << res << endl;
+    }
+
+    else {
+        goto method1;
+    }
+    cout << " Done! " << endl;
+    cout << " VISIT MY GITHUB: Vivmcmrrw " << endl;
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+    cout << " <3 ";
+
+
+}
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
